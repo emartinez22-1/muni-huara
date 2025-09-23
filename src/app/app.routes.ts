8535b6afd7ed243377e890a13e74c0fb6127a1cb
Felipe-Carrasco-1 <carrasco.parra232@gmail.com>
2025-09-23 16:21:17 -0300

    Formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,15 +11,40 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () =>
       import('./pages/home/home.component').then(m => m.HomeComponent),
+    title: 'Inicio | Municipalidad de Huara'
   },
   {
     path: 'contacto',
     loadComponent: () =>
       import('./pages/contacto/contacto.component').then(m => m.ContactoComponent),
+    title: 'Concejales | Municipalidad de Huara'
   },
-  {path:'circulacion', component: PermisosCirculacion},
-  {path:'reuniones', component: ReunionesConsejoMunicipal},
-  {path:'san-lorenzo', component: OrdenanzasSanLorenzo},
-  {path:'ordenanzas-municipales', component: OrdenanzasMunicipales},
+
+
+
+  {path:'circulacion',
+    loadComponent: () =>
+      import('./pages/permisos-circulacion/permisos-circulacion').then(m => m.PermisosCirculacion),
+    title: 'P.Circulacion | Municipalidad de Huara'
+  },
+
+  {path:'reuniones',
+    loadComponent: () =>
+      import('./pages/reuniones-consejo-municipal/reuniones-consejo-municipal').then(m => m.ReunionesConsejoMunicipal),
+    title: 'Reuniones C.M | Municipalidad de Huara'
+  },
+
+  {path:'san-lorenzo',
+    loadComponent: () =>
+      import('./pages/ordenanzas-san-lorenzo/ordenanzas-san-lorenzo').then(m => m.OrdenanzasSanLorenzo),
+    title: 'Ordenanzas S.L | Municipalidad de Huara'
+  },
+
+  {path:'ordenanzas-municipales',
+    loadComponent: () =>
+      import('./pages/ordenanzas-municipales/ordenanzas-municipales').then(m => m.OrdenanzasMunicipales ),
+    title: 'Ordenanzas Municipales | Municipalidad de Huara'
+  },
+
   { path: '**', redirectTo: 'home' },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, AbstractControl, ValidationErrors, FormArray } from '@angular/forms';
 import { NoticiasComponent } from '../../components/noticias/noticias.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { BannersComponent } from '../../components/banners/banners.component';
+import { CommonModule } from '@angular/common';  
+// Validador simple de ejemplo
+function soloLetras(control: AbstractControl): ValidationErrors | null {
+  return /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]+$/.test(control.value || '') ? null : { soloLetras: true };
+}
 
 @Component({
   selector: 'app-home',
@@ -11,7 +17,37 @@ import { BannersComponent } from '../../components/banners/banners.component';
   imports: [
     NoticiasComponent,
     SidebarComponent,
-    BannersComponent
+    BannersComponent,
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
-export class HomeComponent {}
+export class HomeComponent {
+   private fb = inject(FormBuilder);
+ 
+  submitted = false;
+
+  form = this.fb.group({
+    nombre:  ['', [Validators.required, Validators.minLength(2)]],
+    email:   ['', [Validators.required, Validators.email]],
+    mensaje: ['', [Validators.required, Validators.minLength(10)]],
+  });
+
+  get f() { return this.form.controls; }
+
+  enviar() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const data = this.form.getRawValue(); 
+    console.log('SUBMIT -> datos del formulario:', data);
+ 
+
+
+  }
+
+}
